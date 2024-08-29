5d8eaf670d029e4cc8baeb6dc3b1a45a9ddb6cf7
Miguel Salinas <eliassid2@gmail.com>
2024-08-29 15:44:19 -0300

    se agrega ejercicio 1

---
+++ NetBeansProjects/Ejercicio1/src/java/home/RespuestaServlet.java
@@ -0,0 +1,64 @@
+package home;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet que maneja la respuesta y muestra un mensaje de saludo.
+ */
+@WebServlet(name = "RespuestaServlet", urlPatterns = {"/respuesta"})
+public class RespuestaServlet extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Saludo</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h3>Hooola, " + request.getParameter("nombre") + "! </h3>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Servlet que muestra un mensaje de saludo.";
+    }
+}
